06c425d6aa5fde3301ca0a0a9d6c2be1c87bd6bf
hazrafhazwan <muhdhazraf@gmail.com>
2020-11-22 16:20:00 +0000

    Server application

--- 4.2.c
+++ 4.2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>	 //strlen
 #include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
+#include<arpa/inet.h> 	//inet_addr
 #include<unistd.h>	//write
 
 int main(int argc , char *argv[])
